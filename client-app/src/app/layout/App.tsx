5789dec76dc732f8c29e34393cf0cb040cac2d2f
Igor Abilio <igor.abilio@gmail.com>
2020-01-07 17:29:54 +0000

    Finishing 98

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,8 +3,12 @@ import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import LoadingComponent from "./LoadingComponent";
-import ActivityStore from '../stores/activityStore';
-import { observer } from 'mobx-react-lite';
+import ActivityStore from "../stores/activityStore";
+import { observer } from "mobx-react-lite";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 const App = () => {
   const activityStore = useContext(ActivityStore);
@@ -13,13 +17,17 @@ const App = () => {
     activityStore.loadActivities();
   }, [activityStore]);
 
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading activities...' />
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading activities..." />;
 
   return (
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/activities" component={ActivityDashboard} />
+        <Route path="/activities/:id" component={ActivityDetails} />
+        <Route path="/createActivity" component={ActivityForm} />
       </Container>
     </Fragment>
   );
