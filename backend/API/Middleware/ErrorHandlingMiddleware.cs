09ffa3f6ae04a364589b879d059b86289e8777df
Igor Abilio <igor.abilio@gmail.com>
2020-01-08 17:31:36 +0000

    Start implementing handle errors functionality

---
+++ backend/API/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace API.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+    }
+}
